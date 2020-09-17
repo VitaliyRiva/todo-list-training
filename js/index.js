e68a2f401c308360b45705e51d5c4b2146a133ad
VitaliyRiva <v.bilychev@id-east.ru>
2020-09-17 14:46:30 +0300

    del by id

--- js/index.js
+++ js/index.js
@@ -2,48 +2,24 @@
 const todoInput = document.querySelector('.todo-input');
 const entrySection = document.getElementById('entrySection');
 const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list');
 const todoListItem = document.querySelectorAll('.todo-item');
-const todoMark = document.querySelector('.todo-check');
+const todoList = document.querySelector('.todo-list');
 
 
 //Event listeners
 todoButton.addEventListener('click', addTodoHandler);
-
+// todoList.addEventListener('click', markToDo);
 //Functions
 
-const todoElements = [];
+let todoElements = [];
 
 function updateUI() {
   if (todoElements.length === 0) {
-    entrySection.style.display = 'block';
+    entrySection.style.display = 'flex';
   } else {
     entrySection.style.display = 'none';
   }
 };
-
-
-function deleteToDo(todoId) {
-  let identifiedIndex = 0;
-  for (const todo of todoElements) {
-    if (newTodo.id === todoId) {
-      break;
-    }
-    identifiedIndex++;
-  }
-  todoElements.splice(identifiedIndex, 1);
-  todoList.children[identifiedIndex].remove(todoId);
-  updateUI();
-}
-
-
-function deleteHandler(todoId) {
-  let todoTrash = document.querySelector('.todo-trash');
-  todoTrash.replaceWith(todoTrash.cloneNode(true));
-  todoTrash.addEventListener('click', deleteToDo.bind(null, todoId));
-  console.log('deleted');
-};
-
 
 function addTodo(id, title) {
   //Prevent form submitting
@@ -51,23 +27,22 @@ function addTodo(id, title) {
   //Todo Container
   const todoContainer = document.createElement('li');
   todoContainer.classList.add('todo-item');
+  todoContainer.setAttribute('id', id);
   todoContainer.innerHTML = `
-    <span class="todo-info" id="${id}">${title}</span>
+    <span class="todo-info">${title}</span>
     <div class="todo-nav">
       <button class="todo-check">
         <i class="fas fa-check"></i>
       </button>
-      <button class="todo-trash">
+      <button  class="todo-trash">
         <i class="fas fa-trash"></i>
       </button>
     </div>`;
-  todoContainer.addEventListener('click', deleteHandler.bind(null, id));
-  const todoList = document.querySelector('.todo-list');
+  todoList.addEventListener('click', deleteHandler.bind(null, id));
   todoList.appendChild(todoContainer);
-  updateUI();
-
   //Clear Input Value
   todoInput.value = '';
+  updateUI();
 };
 
 
@@ -76,8 +51,8 @@ function addTodoHandler() {
 
   // add object with key and value
   const newTodo = {
-    id: Math.floor(Math.random() * 16 + 1),
-    title: titleValue,
+    id: Math.floor(Math.random() * 10),
+    title: titleValue
   };
 
   //call fn add with params
@@ -89,3 +64,23 @@ function addTodoHandler() {
   updateUI();
 };
 
+
+function deleteTodo(todoId) {
+  let identifiedIndex = 0;
+  for (const el of todoElements) {
+    if (el.id === todoId) {
+      break;
+    }
+    identifiedIndex++;
+  }
+  todoElements.splice(identifiedIndex, 1);
+  todoList.children[identifiedIndex].remove();
+  console.log(todoId);
+  updateUI();
+}
+
+function deleteHandler(todoId) {
+  let todoBtntnTrash = document.querySelector('.todo-trash');
+  todoBtntnTrash.addEventListener('click', deleteTodo.bind(null, todoId));
+  updateUI();
+}
